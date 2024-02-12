354d8a67102cd51056c40e5649c5a13a37eb5835
mithul.se <Mithul@nextgenitltd.com>
2024-02-12 23:54:22 +0600

    signup done

--- src/components/signup.component.tsx
+++ src/components/signup.component.tsx
@@ -1,9 +1,132 @@
+import { useState, useEffect, FormEvent, ChangeEvent } from "react";
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
-    return (
-      <>
-        <h3>React SignUp Component</h3>
-      </>
-    );
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm_password, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
+  let token = localStorage.getItem("token");
+  useEffect(() => {
+    if (token) {
+      // If token exists, navigate to the dashboard
+      navigate("/");
+    }
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
-  
-  export default SignUp;
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmPasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = {
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: confirm_password,
+    };
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const responseData = await response.json();
+
+      if (responseData.errors) {
+        console.log(responseData.errors);
+      } else {
+        console.log(responseData);
+
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="inputName" className="form-label">
+            User Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="inputName"
+            aria-describedby="nameHelp"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <div id="emailHelp" className="form-text">
+            You can give your full name or nickname.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword2" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword2"
+            value={confirm_password}
+            onChange={handleConfirmPasswordChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
